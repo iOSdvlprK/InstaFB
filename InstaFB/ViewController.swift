3d42de0699364442848c4a3727a7f4943f754011
iOS developer K <iOSdvlprK@gmail.com>
2023-01-27 00:48:31 +0900

    Firebase SDK and user registration

--- InstaFB/ViewController.swift
+++ InstaFB/ViewController.swift
@@ -6,6 +6,7 @@
 //
 
 import UIKit
+import Firebase
 
 class ViewController: UIViewController {
     
@@ -22,8 +23,21 @@ class ViewController: UIViewController {
         tf.backgroundColor = UIColor.systemGray6
         tf.borderStyle = .roundedRect
         tf.font = UIFont.systemFont(ofSize: 14)
+        tf.addTarget(nil, action: #selector(handleTextInputChange), for: .editingChanged)
         return tf
     }()
+    
+    @objc func handleTextInputChange() {
+        let isFormValid = emailTextField.text?.count ?? 0 > 0 && usernameTextField.text?.count ?? 0 > 0 && passwordTextField.text?.count ?? 0 > 0
+        
+        if isFormValid {
+            signUpButton.isEnabled = true
+            signUpButton.backgroundColor = UIColor.rgb(red: 17, green: 154, blue: 237)
+        } else {
+            signUpButton.isEnabled = false
+            signUpButton.backgroundColor = UIColor.rgb(red: 149, green: 204, blue: 244)
+        }
+    }
     
     let usernameTextField: UITextField = {
         let tf = UITextField()
@@ -32,6 +46,7 @@ class ViewController: UIViewController {
         tf.backgroundColor = UIColor.systemGray6
         tf.borderStyle = .roundedRect
         tf.font = UIFont.systemFont(ofSize: 14)
+        tf.addTarget(nil, action: #selector(handleTextInputChange), for: .editingChanged)
         return tf
     }()
     
@@ -39,10 +54,11 @@ class ViewController: UIViewController {
         let tf = UITextField()
         tf.textColor = .label
         tf.attributedPlaceholder = NSAttributedString(string: "Password", attributes: [NSAttributedString.Key.foregroundColor: UIColor.systemGray3])
-        tf.isSecureTextEntry = true
+//        tf.isSecureTextEntry = true  // this causes error when connecting auth server
         tf.backgroundColor = UIColor.systemGray6
         tf.borderStyle = .roundedRect
         tf.font = UIFont.systemFont(ofSize: 14)
+        tf.addTarget(nil, action: #selector(handleTextInputChange), for: .editingChanged)
         return tf
     }()
     
@@ -53,8 +69,26 @@ class ViewController: UIViewController {
         button.layer.cornerRadius = 5
         button.titleLabel?.font = UIFont.boldSystemFont(ofSize: 14)
         button.setTitleColor(.white, for: .normal)
+        button.addTarget(nil, action: #selector(handleSignUp), for: .touchUpInside)
+        button.isEnabled = false
         return button
     }()
+    
+    @objc func handleSignUp() {
+//        let email = "dummy@gmail.com"
+//        let password = "123123"
+        guard let email = emailTextField.text, email.count > 0 else { return }
+        guard let username = usernameTextField.text, username.count > 0 else { return }
+        guard let password = passwordTextField.text, password.count > 0 else { return }
+        
+        Auth.auth().createUser(withEmail: email, password: password) { authResult, error in
+            guard let user = authResult?.user, error == nil else {
+                print("Failed to create user:", error!.localizedDescription)
+                return
+            }
+            print("Successfully created user:", user.uid)
+        }
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
