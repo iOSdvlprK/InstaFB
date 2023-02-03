378d2f8d6cbf05b06105b775c54092abbd2e1621
iOS developer K <iOSdvlprK@gmail.com>
2023-02-04 01:25:08 +0900

    user photo selection

---
+++ InstaFB/AddPhoto/PhotoSelectorHeader.swift
@@ -0,0 +1,30 @@
+//
+//  PhotoSelectorHeader.swift
+//  InstaFB
+//
+//  Created by joe on 2023/02/03.
+//
+
+import UIKit
+
+class PhotoSelectorHeader: UICollectionViewCell {
+    
+    let photoImageView: UIImageView = {
+        let iv = UIImageView()
+        iv.contentMode = .scaleAspectFill
+        iv.clipsToBounds = true
+        iv.backgroundColor = .systemCyan
+        return iv
+    }()
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        
+        addSubview(photoImageView)
+        photoImageView.anchor(top: topAnchor, leading: leadingAnchor, bottom: bottomAnchor, trailing: trailingAnchor, paddingTop: 0, paddingLeft: 0, paddingBottom: 0, paddingRight: 0, width: 0, height: 0)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
