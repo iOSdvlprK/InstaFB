dec4430e7e02a352cef74ac47cfdd5db67731a83
iOS developer K <iOSdvlprK@gmail.com>
2023-02-05 02:17:17 +0900

    share post - Storage and DB

--- InstaFB/AddPhoto/SharePhotoController.swift
+++ InstaFB/AddPhoto/SharePhotoController.swift
@@ -6,8 +6,13 @@
 //
 
 import UIKit
+import FirebaseAuth
+import FirebaseStorage
+import FirebaseDatabase
 
 class SharePhotoController: UIViewController {
+    
+    var dbRef: DatabaseReference!
     
     var selectedImage: UIImage? {
         didSet {
@@ -22,6 +27,8 @@ class SharePhotoController: UIViewController {
         navigationItem.rightBarButtonItem = UIBarButtonItem(title: "Share", style: .plain, target: self, action: #selector(handleShare))
         
         setupImageAndTextViews()
+        
+        dbRef = Database.database(url: "https://instafb-58b4d-default-rtdb.asia-southeast1.firebasedatabase.app/").reference()
     }
     
     let imageView: UIImageView = {
@@ -53,6 +60,45 @@ class SharePhotoController: UIViewController {
     }
     
     @objc func handleShare() {
-        print("Sharing photo")
+        guard let caption = textView.text, caption.count > 0 else { return }
+        guard let image = selectedImage else { return }
+        guard let uploadData = image.jpegData(compressionQuality: 0.5) else { return }
+        
+        navigationItem.rightBarButtonItem?.isEnabled = false
+        
+        let filename = UUID().uuidString
+        Storage.storage().reference().child("posts").child(filename).putData(uploadData, metadata: nil) { metadata, err in
+            if let err = err {
+                self.navigationItem.rightBarButtonItem?.isEnabled = true
+                print("Failed to upload post image:", err)
+                return
+            }
+            Storage.storage().reference().child("posts").child(filename).downloadURL { url, err in
+                guard let imageUrl = url?.absoluteString else { return }
+                print("Successfully uploaded post image:", imageUrl)
+                
+                self.saveToDatabaseWithImageUrl(imageUrl: imageUrl)
+            }
+        }
+    }
+    
+    fileprivate func saveToDatabaseWithImageUrl(imageUrl: String) {
+        guard let caption = textView.text else { return }
+        guard let postImage = selectedImage else { return }
+        guard let uid = Auth.auth().currentUser?.uid else { return }
+        
+        let userPostRef = dbRef.child("posts").child(uid)
+        let ref = userPostRef.childByAutoId()
+        let values = ["imageUrl": imageUrl, "caption": caption, "imageWidth": postImage.size.width, "imageHeight": postImage.size.height, "creationDate": Date().timeIntervalSince1970] as [String : Any]
+        
+        ref.updateChildValues(values) { err, ref in
+            if let err = err {
+                self.navigationItem.rightBarButtonItem?.isEnabled = true
+                print("Failed to save post to DB", err)
+                return
+            }
+            print("Successfully saved post to DB")
+            self.dismiss(animated: true, completion: nil)
+        }
     }
 }
